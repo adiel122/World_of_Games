01328b072d60fa012dc7cdcb32991833f2903605
AH <46860117+adiel122@users.noreply.github.com>
2024-07-08 12:31:12 +0300

    Update JenkinsMain.py

--- JenkinsMain.py
+++ JenkinsMain.py
@@ -7,12 +7,6 @@ from JenkinsHelper import print_free_disk_space
 
 print("Adiel is the best!")
 
-# Set up argument parser
-parser = argparse.ArgumentParser(description='Run Jenkins job with specified job name.')
-parser.add_argument('action', type=str, choices=['create_file', 'read_file', 'free_disk', 'move_text', 'schedule'],
-                    help='Action to perform')
-args = parser.parse_args()
-
 jenkins_url = 'http://localhost:8080'
 username = 'admin'
 password_or_api_token = '111e6b82a7320b8ec2c88401592594c337'
@@ -34,20 +28,20 @@ jenkins_manager.create_job(job_name, job_config)
 time.sleep(10)
 jenkins_manager.build_job(job_name)
 
-if args.action == 'create_file':
+if job_name == 'create_file':
     jenkins_manager.create_file_in_workspace(job_name, file_name, 'This file is created during the build job.')
 
-elif args.action == 'read_file':
+elif job_name == 'read_file':
     jenkins_manager.read_file_in_workspace(job_name, file_name)
 
-elif args.action == 'free_disk':
+elif job_name == 'free_disk':
     print_free_disk_space()
 
-elif args.action == 'move_text':
+elif job_name == 'move_text':
     # Implement the logic for move_text
     pass
 
-elif args.action == 'schedule':
+elif job_name == 'schedule':
     # Implement the logic for schedule
     pass
 
